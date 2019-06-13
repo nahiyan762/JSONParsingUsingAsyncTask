8b75795656a8ed180a854c715b9f9e28e51d37cb
Mohammad Nahiyan <nahiyan762@hotmail.com>
2019-06-13 14:32:34 +0600

    AsyncTask Memory Leak issue Solved

--- app/src/main/java/com/example/hp/jsonparsingusingasynctask/MainActivity.java
+++ app/src/main/java/com/example/hp/jsonparsingusingasynctask/MainActivity.java
@@ -1,38 +1,29 @@
 package com.example.hp.jsonparsingusingasynctask;
 
+
 import android.os.AsyncTask;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.JsonReader;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
+import java.lang.ref.WeakReference;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    RecyclerViewAdapter adapter;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,30 +31,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.rview);
-        layoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getBaseContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
 
-        StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        MyTask task=new MyTask();
-        task.execute();
+        MyTask task = new MyTask();
+        task.execute(this);
     }
 
-    class MyTask extends AsyncTask {
+    static class MyTask extends AsyncTask<Object, Object, MainActivity> {
 
-        BufferedReader reader;
+        private BufferedReader reader;
 
         @Override
         protected void onPreExecute() {
-            super.onPreExecute();
+        }
+
+
+        @Override
+        protected MainActivity doInBackground(Object... objects) {
             try {
                 URL url = new URL("https://api.myjson.com/bins/1fi1zm");
 
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                URLConnection connection = url.openConnection();
                 InputStream stream = connection.getInputStream();
-                StringBuilder sb = new StringBuilder();
                 reader = new BufferedReader(new InputStreamReader(stream));
 
             } catch (MalformedURLException e) {
@@ -71,28 +62,26 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-
-        @Override
-        protected Object doInBackground(Object[] objects) {
 
             return null;
         }
 
         @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
+        protected void onPostExecute(MainActivity activity) {
+            WeakReference<MainActivity> contextRef = new WeakReference<>(activity);
+            MainActivity context = contextRef.get();
 
-            Gson gson = new Gson();
-            List<Contact> list = null;
-            try {
-                list = Arrays.asList(gson.fromJson(reader.readLine(),Contact[].class));
-                adapter = new RecyclerViewAdapter(list);
-                recyclerView.setAdapter(adapter);
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (context != null) {
+                Gson gson = new Gson();
+                List<Contact> list;
+                try {
+                    list = Arrays.asList(gson.fromJson(reader.readLine(), Contact[].class));
+                    context.adapter = new RecyclerViewAdapter(list);
+                    context.recyclerView.setAdapter(context.adapter);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
         }
     }
 }
